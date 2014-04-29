33b300ba727a2775f074b035a1cbb13976d57f95
Pascal Ernst <pascal.cc.ernst@gmail.com>
2014-04-29 16:34:06 +0200

    Added base resource blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :blogs
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root "blogs#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
